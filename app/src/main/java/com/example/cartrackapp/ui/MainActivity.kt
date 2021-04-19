e02f00fd2ce77a6a4e4ae3a3dc06612050047109
pandukawedisinghe <panduka.wedisinghe@gmail.com>
2021-04-19 23:00:08 +0800

    Add Uer list and navigation to google maps

--- app/src/main/java/com/example/cartrackapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/cartrackapp/ui/MainActivity.kt
@@ -1,29 +1,45 @@
 package com.example.cartrackapp.ui
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.cartrackapp.R
+import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel by viewModel<MainActivityViewModel>()
+    private val adapter = UserAdapter {
+        val uri: String = java.lang.String.format(Locale.ENGLISH, "geo:%f,%f", it.lat, it.lng)
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+        startActivity(intent)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        userRecyclerView.layoutManager = LinearLayoutManager(this)
+        userRecyclerView.setHasFixedSize(true)
+        userRecyclerView.adapter = adapter
+
         viewModel.onGetUsers.postValue(Unit)
 
         viewModel.run {
             onUsersLoaded.observe(this@MainActivity, Observer {
-                    if (it.isNullOrEmpty()) {
-                        Log.d("Error", "Error")
-                    } else {
-                        val x = it
-                    }
+                if (it.isNullOrEmpty()) {
+                    Toast.makeText(this@MainActivity, "No Users Available", Toast.LENGTH_LONG).show()
+                } else {
+                    adapter.usersList = it
+                    adapter.notifyDataSetChanged()
+                }
             })
         }
     }
---
+++ app/src/main/java/com/example/cartrackapp/ui/UserAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.cartrackapp.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cartrackapp.R
+import com.example.cartrackapp.domain.model.GeoDomain
+import com.example.cartrackapp.domain.model.UserDomain
+import kotlinx.android.synthetic.main.user_list_item_view.view.*
+
+
+class UserAdapter(private val onItemClickListener: ((GeoDomain) -> Unit)) :
+    RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+
+    var usersList = listOf<UserDomain>()
+        set(value) {
+            notifyDataSetChanged()
+            field = value
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val layout = LayoutInflater.from(parent.context).inflate(R.layout.user_list_item_view, parent, false)
+        return UserViewHolder(layout)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        holder.bindData(usersList[position]) {
+            onItemClickListener.invoke(it)
+        }
+    }
+
+    override fun getItemCount(): Int = usersList.count()
+
+    class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val userName: TextView = itemView.userName
+        private val emailTextView: TextView = itemView.emailTextView
+        private val addressTextView: TextView = itemView.addressTextView
+        private val phoneTextView: TextView = itemView.phoneTextView
+
+        fun bindData(user: UserDomain, onItemClickListener: ((GeoDomain) -> Unit)?) {
+            val address = user.address
+            userName.text = user.name
+            emailTextView.text = user.email
+            addressTextView.text = address.suite + "," + address.street + "," + address.city
+            phoneTextView.text = user.phone
+            itemView.setOnClickListener { onItemClickListener?.invoke(address.geo) }
+        }
+    }
+}
